1b89e58eeb176fdd66afe930501d9433206d3ff8
Muneer Ahmed <muneer.ahmed@servicemax.com>
2020-11-12 17:51:05 -0800

    Used exchange with flatMap

--- ReadMe.txt
+++ ReadMe.txt
@@ -1,2 +1,10 @@
-curl -v http://localhost:8080/web/file/nio
-curl -v --connect-timeout 1 -m 100 http://localhost:8080/web/file/nio
+curl -v http://localhost:8080/web/file/nio?uri=https://www.w3.org/TR/PNG/iso_8859-1.txt
+curl -v http://localhost:8080/web/file/nio?uri=http://www.sci.utah.edu/~macleod/docs/txt2html/sample.txt
+curl -v http://localhost:8080/web/file/nio?uri=http://ipv4.download.thinkbroadband.com/5MB.zip > output.zip
+curl -v http://localhost:8080/web/file/nio?uri=http://ipv4.download.thinkbroadband.com/512MB.zip > output.zip
+
+curl -v --connect-timeout 1 -m 100 http://localhost:8080/web/file/nio?size=512
+
+    private static final String TEXT_FILE_URL = "http://www.sci.utah.edu/~macleod/docs/txt2html/sample.txt";
+    private static final String ZIP_FILE_URL = "http://ipv4.download.thinkbroadband.com/5MB.zip";
+    private static final String LOCATION = TEXT_FILE_URL;
--- src/main/java/com/example/proxy/NonBlockingProxy.java
+++ src/main/java/com/example/proxy/NonBlockingProxy.java
@@ -1,18 +1,22 @@
 package com.example.proxy;
 
 import java.io.IOException;
+import java.io.OutputStream;
 import java.nio.ByteBuffer;
+import java.util.concurrent.atomic.AtomicBoolean;
 import javax.servlet.AsyncContext;
+import javax.servlet.AsyncEvent;
+import javax.servlet.AsyncListener;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.io.buffer.DataBuffer;
 import org.springframework.core.io.buffer.DataBufferUtils;
-import org.springframework.http.MediaType;
 import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.reactive.function.client.ClientResponse;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Flux;
@@ -29,55 +33,88 @@ public class NonBlockingProxy {
                     .create()
                     .wiretap(false);
         this.webClient = webClientBuilder
-                            .baseUrl("http://ipv4.download.thinkbroadband.com")
                             .clientConnector(new ReactorClientHttpConnector(httpClient))
                             .build();
     }
 
     private final WebClient webClient;
-    private static final String TEXT_FILE_URL = "http://www.sci.utah.edu/~macleod/docs/txt2html/sample.txt";
-    private static final String ZIP_FILE_URL = "http://ipv4.download.thinkbroadband.com/5MB.zip";
-    private static final String LOCATION = TEXT_FILE_URL;
+
+    static class AsyncCompletedListener implements AsyncListener {
+
+        private final AtomicBoolean completed = new AtomicBoolean();
+
+        public boolean isCompleted() {
+            return completed.get();
+        }
+
+        @Override
+        public void onComplete(AsyncEvent event) {
+            log.error("Listener Request is Completed");
+            completed.set(true);
+        }
+
+        @Override
+        public void onTimeout(AsyncEvent event) {
+            log.error("timed out while proxying {}", event.getThrowable());
+            completed.set(true);
+        }
+
+        @Override
+        public void onError(AsyncEvent event) {
+            log.error("error occurred while proxying {}", event.getThrowable());
+            completed.set(true);
+        }
+
+        @Override
+        public void onStartAsync(AsyncEvent event) {
+
+        }
+    }
 
     @GetMapping("/nio")
     public void nioFile(HttpServletRequest request,
-                        HttpServletResponse response) {
-        log.debug("Starting Worker Thread for /nio !");
+                        HttpServletResponse response,
+                        @RequestParam("uri") String uri) throws IOException {
+        log.debug("Received request for download file");
+        OutputStream outputStream = response.getOutputStream();
         AsyncContext asyncContext = request.startAsync(request, response);
+        AsyncCompletedListener asyncCompletedListener = new AsyncCompletedListener();
+        asyncContext.addListener(asyncCompletedListener);
         webClient.get()
-                .uri("/5MB.zip")
-                .accept(MediaType.APPLICATION_OCTET_STREAM)
+                .uri(uri)
                 .exchange()
-                .doOnSuccess((ClientResponse clientResponse) -> {
-                    log.debug("Writing Response Async Worker Thread !");
+                .flatMap(clientResponse -> {
+                    log.debug("Handling Response");
+                    response.setStatus(clientResponse.rawStatusCode());
                     copyHeaders(clientResponse, response);
-                    copyBody(asyncContext, clientResponse, response);
+                    Mono<Void> mono = copyBody(clientResponse, outputStream);
+                    log.debug("Handling Response Completed");
+                    return mono;
                 })
+                .doFinally(signalType ->  {
+                        log.debug("Completed Request");
+                        if(!asyncCompletedListener.isCompleted()) {
+                            asyncContext.complete();
+                        }
+                    })
                 .subscribe();
-        log.debug("Finished Worker Thread for /nio !");
     }
 
     private void copyHeaders(ClientResponse fileResponse, HttpServletResponse response) {
-        log.debug("Writing Response Headers !");
+        log.debug("Writing Response Headers");
         fileResponse.headers()
                     .asHttpHeaders()
-                    .forEach((k, v) ->  response.addHeader(k, v.get(0)));
+                    .forEach((k, v) ->  v.forEach(e -> {
+                        response.addHeader(k, e);
+                        // log.debug("key='{}' value='{}'", k, e);
+                    } ) );
     }
 
-    private void copyBody(AsyncContext asyncContext, ClientResponse fileResponse, HttpServletResponse response) {
-        log.debug("Writing Response Body !");
-
-        Flux<DataBuffer> flux = fileResponse.bodyToFlux(DataBuffer.class);
-        try {
-            DataBufferUtils.write(flux, response.getOutputStream())
-                            .limitRate(1000)
-                            .log()
-                            .subscribe(e -> {
-                                DataBufferUtils.release(e);
-                            });
-        } catch (IOException e) {
-            e.printStackTrace();
-            throw new RuntimeException(e);
-        }
+    private Mono<Void> copyBody(ClientResponse fileResponse, OutputStream outputStream) {
+        log.debug("Writing Response Body");
+        Flux<DataBuffer> buffers = fileResponse.bodyToFlux(DataBuffer.class).limitRate(10);
+        return DataBufferUtils.write(buffers, outputStream)
+                .map(DataBufferUtils::release)
+                .then();
     }
 }
