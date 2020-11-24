049ef990e480df32d47fd5c8652eb86952bc6762
Muneer Ahmed <muneer.ahmed@servicemax.com>
2020-11-24 14:45:37 -0800

    Added documentation

--- src/main/java/com/spring/example/webflux/DateTimeService.java
+++ src/main/java/com/spring/example/webflux/DateTimeService.java
@@ -10,8 +10,35 @@ import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 /**
- * WebClient tutorial https://www.callicoder.com/spring-5-reactive-webclient-webtestclient-examples/
  *
+ * Terminology
+ *
+ * 	• Design Patterns : Observer able or Producers, Observer or Subscribers and Iterator
+ *
+ * 	• Publisher
+ * 	• Subscriber
+ * 	• Subscription
+ * 	• Processor
+ *
+ * 	• Mono
+ * 	• Flux
+ * 	• Operators
+ * 	• Pure Functions
+ * 	• Pipeline
+ *
+ * 	• Mono or Flux is just a function which iterates over the data and push the value to Observer and returns Subscription so that consumer can say “i don't need more data”.
+ * 	• Basically convert the data producer to an Observable
+ * 	• Data producer can be anything for eg: http call, database call etc.
+ * 	• In Short we need to find data sources API which returns observable rather than we doing ourselves as its not a trivial task.
+ * 	• Once we get observable consumer need to subscribe to it in order for the data producer to start producing values. Hence we need to subscribe to the observable.
+ * 	• Web Client converts HTTP Call to an observable either Mono or Flux
+ *
+ * 	In nutshell
+ * 	• An Observable is just a function , consumer call it by subscribing to it and then observable or producer will start producing values.
+ *
+ * 	References:
+ * 	• WebClient tutorial https://www.callicoder.com/spring-5-reactive-webclient-webtestclient-examples/
+ * 	• How to Create Mono/Flux https://medium.com/@cheron.antoine/reactor-java-1-how-to-create-mono-and-flux-471c505fa158
  */
 
 @Service
